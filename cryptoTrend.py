7b15126c18b96a2eb3487480d594ba5a7464c93a
unknown <maartinschultz@gmail.com>
2021-12-21 20:37:57 +0100

    fonction getTopics optimiser

--- cryptoTrend.py
+++ cryptoTrend.py
@@ -7,12 +7,11 @@ def getTopics():
     session = HTMLSession()
     url = session.get("https://www.jeuxvideo.com/forums/0-3011927-0-1-0-1-0-finance.htm")
     topics = url.html.find("a.topic-title")
-    topics=topics[4:5] # récupére la partie titres des topics
+    # récupére la partie titres des topics
     
-    for topic in topics:
-        BlocTopicBrut = topic.text[0:topic.text.find("Page suivante")]
-        #topic.text.find("Page suivante") #renvoi la fin de la listes des topics
-    
+    #4ème titre liste les topics de la page
+    BlocTopicBrut = topics[4].text[0:topics[4].text.find("Page suivante")]
+    #topic.text.find("Page suivante") #renvoi la fin de la listes des topics
     listeTopicBrut = re.split('\n',BlocTopicBrut)
     return listeTopicBrut
 
@@ -22,7 +21,6 @@ def getUrlTopics():
     e=[]
     for match in re.finditer("<a class=\"lien-jv topic-title\" href=\"", url.text):
          e.append(match.end())
-    
     if (len(e)==25):# car JVC n'affcihe que les 25 plus récent forum #on s'assure de bien les avoir
         listeUrlTopic=[]
         for i in range(len(e)):
@@ -37,6 +35,14 @@ def getpostsTopic(urlTopic):#retourne toute les posts d'un topic passer en param
     posts = url.html.find(".bloc-message-forum")
     listeposteBrut = re.split('\n',posts[0].text[:posts[0].text.find("Page suivante")-1])
     # tout les posts sont contenus dans posts[0s]
-    
     return listeposteBrut
-    
+
+
+#print(getpostsTopic("https://www.jeuxvideo.com/forums/42-3011927-62814144-1-0-1-0-terra-luna.htm"))
+
+print(getTopics())
+
+
+
+
+
