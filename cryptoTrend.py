1990c4c76db56b33bec6cf171c8b4a6da20f7514
unknown <maartinschultz@gmail.com>
2021-12-21 21:51:51 +0100

    données retournées gettopics

--- cryptoTrend.py
+++ cryptoTrend.py
@@ -10,10 +10,18 @@ def getTopics():
     # récupére la partie titres des topics
     
     #4ème titre liste les topics de la page
-    BlocTopicBrut = topics[4].text[0:topics[4].text.find("Page suivante")]
+    BlocTopicBrut = topics[4].text[0:topics[4].text.find("Page suivante")-1]
     #topic.text.find("Page suivante") #renvoi la fin de la listes des topics
     listeTopicBrut = re.split('\n',BlocTopicBrut)
-    return listeTopicBrut
+    
+    listeTopic = []
+    for topic in listeTopicBrut:
+        e=[]
+        for match in re.finditer(" ", topic):
+            e.append(match.start())
+        listeTopic.append(topic[0:e[-3]])#enlève les 3 derniers qui ne sont pas un titre
+        #e[-1] date dernier msg  | e[-2] nb msg | e[-3] auteur topic
+    return listeTopic
 
 def getUrlTopics():    
     session = HTMLSession()
@@ -28,21 +36,16 @@ def getUrlTopics():
             listeUrlTopic.append('https://www.jeuxvideo.com/ '+link)
         return listeUrlTopic
 
-def getpostsTopic(urlTopic):#retourne toute les posts d'un topic passer en paramètre    
+def getPostsTopic(urlTopic):#retourne toute les posts d'un topic passer en paramètre    
     #urlTopic="https://www.jeuxvideo.com/forums/42-3011927-68489642-1-0-1-0-ca-marche-kda-ou-pas.htm"
     session = HTMLSession()
     url = session.get(urlTopic)
-    posts = url.html.find(".bloc-message-forum")
-    listeposteBrut = re.split('\n',posts[0].text[:posts[0].text.find("Page suivante")-1])
-    # tout les posts sont contenus dans posts[0s]
-    return listeposteBrut
+    posts = url.html.find("div.txt-msg")
 
-
+    listePostBrut = posts[0].text[:posts[0].text.find("Page suivante")-1]
+    listePostBrut = re.split('\n',listePostBrut)
+    # tout les posts sont contenus dans posts[0]
+    return listePosteBrut
 #print(getpostsTopic("https://www.jeuxvideo.com/forums/42-3011927-62814144-1-0-1-0-terra-luna.htm"))
-
-print(getTopics())
-
-
-
 
 
