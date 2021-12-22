0d749d327b6a894831dd5c5be02a0d81bebe7ebc
unknown <maartinschultz@gmail.com>
2021-12-22 15:43:23 +0100

    Extraction des clés crypto

--- cryptoDataJVC.py
+++ cryptoDataJVC.py
@@ -9,18 +9,21 @@ def getTopics():
     topics = url.html.find("a.topic-title")
     # récupére la partie titres des topics
     
-    #4ème titre liste les topics de la page
+    #4ème topic liste les topics de la page
     BlocTopicBrut = topics[4].text[0:topics[4].text.find("Page suivante")-1]
     #topic.text.find("Page suivante") #renvoi la fin de la listes des topics
-    listeTopicBrut = re.split('\n',BlocTopicBrut)
     
+    listeTopicBrut = re.split('\n',BlocTopicBrut)
     listeTopic = []
     for topic in listeTopicBrut:
         e=[]
         for match in re.finditer(" ", topic):
             e.append(match.start())
-        listeTopic.append(topic[0:e[-3]])#enlève les 3 derniers qui ne sont pas un titre
+        topic = topic[0:e[-3]] #enlève les 3 derniers mots qui ne sont pas un titre
         #e[-1] date dernier msg  | e[-2] nb msg | e[-3] auteur topic
+        for word in re.split("\[|\]| ", topic): #
+            if len(word)>2:
+                listeTopic.append(word)       
     return listeTopic
 
 def getUrlTopics():    
@@ -37,7 +40,6 @@ def getUrlTopics():
         return listeUrlTopic
 
 def getPostsTopic(urlTopic):#retourne toute les posts d'un topic passer en paramètre    
-    #urlTopic="https://www.jeuxvideo.com/forums/42-3011927-68489642-1-0-1-0-ca-marche-kda-ou-pas.htm"
     session = HTMLSession()
     url = session.get(urlTopic)
     posts = url.html.find("div.txt-msg")
@@ -45,5 +47,14 @@ def getPostsTopic(urlTopic):#retourne toute les posts d'un topic passer en param
     listePostBrut = posts[0].text[:posts[0].text.find("Page suivante")-1]
     listePostBrut = re.split('\n',listePostBrut)
     # tout les posts sont contenus dans posts[0]
-    return listePostBrut
+    
+    listePost = []
+    for post in listePostBrut:
+        for word in re.split(" ", post):
+            if len(word)>2:
+                listePost.append(word)           
+    return listePost
+
+
+
 
--- cryptoFindKey.py
+++ cryptoFindKey.py
@@ -1,7 +1,22 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Dec 22 10:28:40 2021
+import re
 
-@author: Maartin
-"""
+def getCryptoKey(listemots):
+    regexCrypto = "^[A-Z]{3,12}$"
+    #"(^[[a-z]{3,12}]$|^[[A-Z]{3,12}]$|^[A-Z]{3,12}$)"
+    
+    clesCrypto=[]
+    for cles in listemots:
+        #re.search(regexCrypto, cles)
+        # on vérfie qu'une chaine est à le format d'une clé de cryptomonnaie
+        if(re.search(regexCrypto, cles)):
+            clesCrypto.append(cles)
+    return clesCrypto
+
+
+# ## EXEMPLE ##
+# import cryptoDataJVC
+# url = "https://www.jeuxvideo.com/forums/42-3011927-68193322-1-0-1-0-ceek-vr-meta-space-x-nasa-votre-excuse-pour-ne-pas-monter-dans-le-train.htm"
+# x = cryptoDataJVC.getPostsTopic(url)
+# y=getCryptoKey(x)
+# print(y)
 
