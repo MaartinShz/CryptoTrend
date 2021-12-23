969406c342549f0c96963480ffbd163022ca17d9
Antoine-gird <82816589+Antoine-gird@users.noreply.github.com>
2021-12-23 17:23:59 +0100

    Add files via upload

--- coinmarketcap.py
+++ coinmarketcap.py
@@ -1,49 +1,84 @@
-tab = []
-
-
 from requests import Request, Session
 from requests.exceptions import ConnectionError, Timeout, TooManyRedirects
 import json
 
-url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
-parameters = {
-  'start':'1',
-  'limit':'10',
-  'convert':'USD'
-}
-headers = {
-  'Accepts': 'application/json',
-  'X-CMC_PRO_API_KEY': '994352b1-f247-4a59-82e4-0f117bf65dbf',
-}
+#---------------------------
+#fonction validCrypto : si la crypto existe, on récupère ses données
+#---------------------------
 
-session = Session()
-session.headers.update(headers)
+def validCrypto(symbole):
+    url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest'
+    parameters = {
+        'symbol':symbole,    
+        'convert':'USD',                    
+        }
+    headers = {
+        'Accepts': 'application/json',
+        'X-CMC_PRO_API_KEY': '994352b1-f247-4a59-82e4-0f117bf65dbf',
+    }
+    
+    session = Session()
+    session.headers.update(headers)
+    
+    try:
+      response = session.get(url, params=parameters)
+      donnees = json.loads(response.text)
+      #print(donnees)
+      return(donnees)
+    except (ConnectionError, Timeout, TooManyRedirects) as erreur:
+      print(erreur)
 
-try:
-  response = session.get(url, params=parameters)
-  data = json.loads(response.text)
-  print(data)
-except (ConnectionError, Timeout, TooManyRedirects) as e:
-  print(e)
+#---------------------------
+#On récupère la liste des cryptos de JVC
+#---------------------------
 
-#recherche : 
-# mcap < 100
-# re nom
+from Crypto import Crypto
+import cryptoDataJVC
+import cryptoFindKey
 
-data["data"]
+url = "https://www.jeuxvideo.com/forums/42-3011927-68193322-1-0-1-0-ceek-vr-meta-space-x-nasa-votre-excuse-pour-ne-pas-monter-dans-le-train.htm"
+x = cryptoDataJVC.getPostsTopic(url)
+listeCrypto=cryptoFindKey.getCryptoKey(x)
+listeCrypto
+# /!\ SUPPRIMER LES DOUBLONS
 
-id =[data['id'] for data in data["data"]]
-id
+#---------------------------
+#On créé les cryptos existantes que l'on ajoute à la liste des cryptos
+#--------------------------- 
 
-nom =[data['name'] for data in data["data"]]
-nom
+liste_crypto=[]
 
-symbol =[data['symbol'] for data in data["data"]]
-symbol
-
-marketcap =[data["quote"]["USD"]["market_cap"] for data in data["data"]]
-marketcap
-
+for symbol in listeCrypto:
+    nomSymbol=symbol
+    test=validCrypto(nomSymbol)
+    test
+    try:
+        if(test['data'][nomSymbol]['quote']['USD']['market_cap']<1000000000): #marketcap<1 milliard (voir si on garde cette valeur)
+            cle=test['data'][nomSymbol]['id']
+            nom=test['data'][nomSymbol]['name']
+            marketcap=test['data'][nomSymbol]['quote']['USD']['market_cap']
+            prix=test['data'][nomSymbol]['quote']['USD']['price']
+            launch=test['data'][nomSymbol]['date_added'] #recoder la date ?
+            localisation=test['data'][nomSymbol]['platform']['symbol']
+            new_crypto = Crypto(cle, nom, marketcap, prix, launch, localisation)
+            liste_crypto.append(new_crypto)
+            print("---------------------------------------------")
+            print("Crypto ajoutée ("+nomSymbol+")")
+            print("---------------------------------------------")
+        else:
+            print("---------------------------------------------")
+            print("MarketCap déjà élevé : cela ne nous intéresse pas")
+            print("---------------------------------------------")
+    except KeyError:
+        print("---------------------------------------------")
+        print("Cette crypto n'existe pas ("+nomSymbol+")")
+        print("---------------------------------------------")
+    except TypeError:
+        print("---------------------------------------------")
+        print("Information manquante")
+        print("---------------------------------------------")
+        
+  
 
 
 
