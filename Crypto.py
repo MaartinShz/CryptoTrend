c6be8ec2f6e91ebf19e07e76d0b7a3c7047f7e52
Antoine-gird <82816589+Antoine-gird@users.noreply.github.com>
2022-01-02 15:39:50 +0100

    Add files via upload

--- Crypto.py
+++ Crypto.py
@@ -1,18 +1,37 @@
 class Crypto:
-  def __init__(self, cle, nom, marketCap, price, launch, localisation):
-    self.cle = cle
-    self.nom = nom
-    self.marketCap = marketCap
+    def __init__(self, cle, nom, marketCap, price, launch, localisation):
+        self.cle = cle
+        self.nom = nom
+        self.marketCap = marketCap
+        self.price = price
+        self.launch = launch
+        self.localisation = localisation
     
-    self.price = price
-    self.launch = launch
-    self.localisation = localisation
-    
-    self.corpus = []
-    
+        self.corpus = []
     def __str__(self):
-        return f'Crypto({self.cle},{self.nom},{self.marketCap})'
+       return f"{self.cle}, {self.nom}, {self.marketCap}"  
+     # Fonction qui renvoie le texte à afficher lorsqu'on tape repr(classe)
+    def __repr__(self):
+        return f"{self.cle}, {self.nom}, {self.marketCap}" 
+    
+    def get_cle(self):
+        return self.cle
+    
+    def get_nom(self):
+        return self.nom
+    
+    def get_marketCap(self):
+        return self.marketCap
+    
+    def get_price(self):
+        return self.price
 
+    def get_launch(self):
+        return self.launch
+    
+    def get_localisation(self):
+        return self.localisation
+    
 class Texte:
     def __init__(self, id, url, source):
         self.id = id
@@ -29,7 +48,7 @@ class TexteReddit(Texte):
     def __str__(self):
         return f'TexteReddit({self.id}, {self.source}, {self.upvote})'
 
-# test = Crypto('cle', 'nom', 'marketCap', 'price', 'launch', 'localisation')
-# test = Texte('id', 'url', 'source')
-# test = TexteReddit('id', 'url', 'source', 'upvote', 'nbCommentaire')
-# print(test)
+#test = Crypto('cle', 'nom', 'marketCap', 'price', 'launch', 'localisation')
+#test = Texte('id', 'url', 'source')
+#test = TexteReddit('id', 'url', 'source', 'upvote', 'nbCommentaire')
+#print(test)
